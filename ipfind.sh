c497f49981ead0a143691ddbe03045e635cd73bf
M0rph3u5 <divyanshushukl@gmail.com>
2017-01-25 18:39:50 +0530

    Add files via upload

---
+++ ipfind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat /home/morpheus/iplist.txt | awk &#39;{print $Z}&#39; &gt; ipformat.txt
+
+ipformat.txt|uniq &gt; mainiplist.txt
+
+IP=`cat mainiplist.txt`
+
+for i in $IP
+
+do
+
+echo &quot;$i,`geoiplookup $i | cut -d &quot;,&quot; -f2 | sed -e &#39;s&#39;`&quot; &gt;&gt; info.txt
+
+done
